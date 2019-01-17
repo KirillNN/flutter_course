cc03ea4a84ea03699af2967abca788551d231296
PKS <kirill_nn@inbox.ru>
2019-01-17 14:31:27 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import './product_manager.dart';
+
+void main() {
+  debugPaintBaselinesEnabled = true;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManager('Food Tester'),
+      ),
+    );
+  }
+}
