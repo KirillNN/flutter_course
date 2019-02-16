3650f4704331e53c9e2a4cf0fd57149286874e38
pekse <kirill_nn@inbox.ru>
2019-02-16 18:47:55 +0300

    6-72

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
@@ -8,25 +10,32 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(imageUrl),
-          Container(padding: EdgeInsets.all(10.0), child: Text(title)),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: RaisedButton(
-              onPressed: () => Navigator.pop(context, true),
-              child: Text(
-                'DELETE',
+    return WillPopScope(
+      onWillPop: () {
+        print('Back buttom pressed!');
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(imageUrl),
+            Container(padding: EdgeInsets.all(10.0), child: Text(title)),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () => Navigator.pop(context, true),
+                child: Text(
+                  'DELETE',
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
