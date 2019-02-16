3c54093cd5003fbb9cc60b52f120e2252c98c496
pekse <kirill_nn@inbox.ru>
2019-02-16 12:09:13 +0300

    6-69

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -4,19 +4,27 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Product Detail'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Text('On the Product Page'),
-            RaisedButton(
+      appBar: AppBar(
+        title: Text('Product Detail'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text('On the Product Page')),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: RaisedButton(
               onPressed: () => Navigator.pop(context),
               child: Text(
                 'BACK',
               ),
-            )
-          ],
-        ));
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
