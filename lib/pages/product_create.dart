e80a3c82a35ce04550a8de8ec4b690b7776075a8
PKS <kirill_nn@inbox.ru>
2019-02-19 08:34:06 +0300

    7-89

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -8,7 +8,9 @@ class ProductCreatePage extends StatefulWidget {
 }
 
 class ProductCreatePageState extends State<ProductCreatePage> {
-  String titleValue = '';
+  String titleValue;
+  String descriptionValue;
+  double priceValue;
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +23,21 @@ class ProductCreatePageState extends State<ProductCreatePage> {
             });
           },
         ),
-        Text(titleValue),
+        TextField(maxLength: 4,
+          onChanged: (String value) {
+            setState(() {
+              descriptionValue = value;
+            });
+          },
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          onChanged: (String value) {
+            setState(() {
+              priceValue = double.parse(value);
+            });
+          },
+        )
       ],
     );
   }
