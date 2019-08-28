4f8c4bfb3ca2c1e33cc2b6b84968da8573bda095
PKS <kirill_nn@inbox.ru>
2019-08-28 09:41:08 +0300

    Upd

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,31 @@
 import 'package:flutter/material.dart';
 
-import './pages/auth.dart';
-import './pages/product.dart';
-import './pages/product_admin.dart';
-import './pages/products.dart';
-// import 'package:flutter/rendering.dart';
-
 void main() {
-//   debugPaintSizeEnabled = true;
-  // debugPaintBaselinesEnabled = true;
-  // debugPaintPointersEnabled = true;
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
+  _State createState() => _State();
 }
 
-class _MyAppState extends State<MyApp> {
-  List<Map<String, dynamic>> _products = [];
-
-  void _addProduct(Map<String, dynamic> product) {
-    setState(() {
-      _products.add(product);
-    });
-  }
-
-  void _deleteProduct(int index) {
-    setState(() {
-      _products.removeAt(index);
-    });
-  }
-
+class _State extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-// debugShowMaterialGrid: true,
-      theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: Colors.deepOrange,
-          accentColor: Colors.deepPurple),
-      // home: AuthPage(),
-      routes: {
-        '/': (BuildContext context) => AuthPage(),
-        '/products': (BuildContext context) => ProductsPage(
-              _products,
-            ),
-        '/admin': (BuildContext context) =>
-            ProductAdminPage(_addProduct, _deleteProduct),
-      },
-      onGenerateRoute: (RouteSettings setting) {
-        final List<String> pathElements = setting.name.split('/');
-        if (pathElements[0] != '') {
-          return null;
-        }
-        if (pathElements[1] == 'product') {
-          final int index = int.parse(pathElements[2]);
-          return MaterialPageRoute<bool>(
-            builder: (BuildContext context) => ProductPage(
-                _products[index]['title'],
-                _products[index]['image'],
-                _products[index]['description'],
-                _products[index]['price']),
-          );
-        }
-        return null;
-      },
-      onUnknownRoute: (RouteSettings setting) {
-        return MaterialPageRoute(
-          builder: (BuildContext context) => ProductsPage(_products),
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Name here'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[Text('Add Widgets Here')],
+          ),
+        ),
+      ),
     );
   }
 }
