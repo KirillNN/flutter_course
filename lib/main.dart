9e725bf671b269f520ff360878e47672a1354721
Kirill <kirill_nn@inbox.ru>
2019-09-04 20:24:51 +0300

    2-24

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: MyApp(),
-  ));
-}
+void main() {}
 
-class MyApp extends StatefulWidget {
-  @override
-  _State createState() => _State();
-}
-
-class _State extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Name here'),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(32.0),
-        child: Center(
-          child: Column(
-            children: <Widget>[Text('Add Widgets Here')],
-          ),
-        ),
-      ),
+    return MaterialApp(
+      home: Text('Hello'),
     );
   }
 }
